86910b0d85e7a64169e237dc463b22a08156a957
Yansel93 <yhmarzo@gmail.com>
2024-08-06 14:45:34 -0400

    agregado el codigo de barra

--- ERP_Project/admin.py
+++ ERP_Project/admin.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 from django.contrib import admin
-from .models import Producto, Categoria, Inventario
+from .models import Producto, Categoria, Inventario, Registro_de_altas, Registro_de_bajas
 
 
 # Register your models here.
@@ -14,6 +14,18 @@ class ProductoAdmin(admin.ModelAdmin):
 
 admin.site.register(Categoria)
 
-
 admin.site.register(Inventario)
 
+
+@admin.register(Registro_de_altas)
+class Registro_de_altasAdmin(admin.ModelAdmin):
+    list_display = ('cantidad', 'fecha')
+    list_display_links = ('cantidad', 'fecha')
+    list_display_links_limit = 2
+
+
+@admin.register(Registro_de_bajas)
+class Registro_de_bajasAdmin(admin.ModelAdmin):
+    list_display = ('cantidad', 'fecha')
+    list_display_links = ('cantidad', 'fecha')
+    list_display_links_limit = 2
--- ERP_Project/models.py
+++ ERP_Project/models.py
@@ -1,5 +1,10 @@
 import random
 from django.db import models
+from barcode import get_barcode_class
+from barcode.writer import ImageWriter
+from django.core.files.base import ContentFile
+from io import BytesIO
+
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
@@ -12,18 +17,33 @@ class Categoria(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     abreviaciones = models.CharField(max_length=10)
-    codigo = models.CharField(max_length=255)
-    codigo_barra = models.CharField(max_length=255)
+    categoria = models.ForeignKey(Categoria, null=True, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=255, null=True, blank=True)
+    codigo_barra = models.ImageField(upload_to='codigos_barra', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
 
     def save(self, *args, **kwargs):
         # Generar un número aleatorio entre 1 y 999
-        random_number = random.randint(1, 999)
+        random_number = random.randint(1, 999999999999)
 
-        # Concatenar el código existente con el número aleatorio
-        self.codigo = f"{self.abreviaciones}-{random_number:03d}"
+        # Convertir el número aleatorio a una cadena de 12 dígitos con ceros a la izquierda
+        self.codigo = f"{self.abreviaciones}-{random_number:012d}"
+
+        try:
+            # Generar el código de barras
+            EAN = get_barcode_class('ean13')
+            ean = EAN(str(random_number), writer=ImageWriter())
+            barcode_image = ean.render()
+            # Guarda la imagen del código de barras en el campo
+            image_io = BytesIO()
+            barcode_image.save(image_io, format='PNG')
+            self.codigo_barra.save(f"{self.codigo}.png", ContentFile(image_io.getvalue()), save=False)
+        except (ValueError, RuntimeError) as e:
+            # Si hay un error, dejar el campo codigo_barra como None
+            self.codigo_barra = None
+            print(f"Error generando código de barras: {e}")
 
         super().save(*args, **kwargs)
 
@@ -31,9 +51,18 @@ class Producto(models.Model):
 # This is model of inventary
 class Inventario(models.Model):
     produto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
-    alta = models.IntegerField()
-    baja = models.IntegerField()
+    cantidad = models.IntegerField()
 
 
+# This model is for taking the products that arrive at the warehouse
+class Registro_de_altas(models.Model):
+    produto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha = models.DateField()
 
 
+# This model is for taking the products who are deregistered from the warehouse
+class Registro_de_bajas(models.Model):
+    produto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha = models.DateField()
