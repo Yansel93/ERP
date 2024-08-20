3af5d37424bb5747fe66383b75422e9513d1b262
Yansel93 <yhmarzo@gmail.com>
2024-08-20 19:13:02 -0400

    cambios en el inventario

--- ERP_Project/admin.py
+++ ERP_Project/admin.py
@@ -1,38 +1,73 @@
 from typing import Tuple
-
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
 from .models import Producto, Categoria, Inventario, Registro_de_altas, Registro_de_bajas
 
 
 # Register your models here.
-@admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    fields = ['nombre', 'abreviaciones']
-    list_display = ('nombre', 'abreviaciones', 'codigo', 'codigo_barra')
+    fields = ['nombre', 'abreviaciones', 'categoria', 'iba']
+    list_display = ('nombre', 'abreviaciones', 'categoria', 'mostrar_iba', 'codigo', 'codigo_barra')
     list_display_links = ('nombre', 'abreviaciones')
+    list_filter = ('nombre',)
+    search_fields = ('nombre',)
     list_display_links_limit = 2
 
+    def mostrar_iba(self, obj):
+        return f"{obj.iba}%"
 
+    mostrar_iba.short_description = "IBA (%)"
+
+
+admin.site.register(Producto, ProductoAdmin)
 
 admin.site.register(Categoria)
 
 
-@admin.register(Inventario)
 class InventarioAdmin(admin.ModelAdmin):
-    list_display = ('producto', 'cantidad')
+    list_display = ('producto', 'cantidad', 'mostrar_iba', 'mostrar_UM')
     list_display_links = ('producto', 'cantidad')
+    list_filter = ('producto',)
+    search_fields = ('producto__nombre',)
     list_display_links_limit = 2
 
+    def mostrar_iba(self, obj):
+        return f"{obj.producto.iba}%"
 
-@admin.register(Registro_de_altas)
+    mostrar_iba.short_description = "IBA (%)"
+
+    def mostrar_UM(self, obj):
+        return f"{obj.unidad_medida}"
+
+    mostrar_UM.short_description = "UM"
+
+
+admin.site.register(Inventario, InventarioAdmin)
+
+
 class Registro_de_altasAdmin(admin.ModelAdmin):
-    list_display = ('producto', 'cantidad', 'fecha')
+    fields = ['producto', 'cantidad','valor_unitario', 'fecha']
+    list_display = ('producto', 'cantidad', 'get_iba_display', 'fecha','valor_unitario', 'importe_total')
     list_display_links = ('cantidad', 'producto')
+    list_filter = ('producto', 'fecha')
+    search_fields = ('producto__nombre',)
     list_display_links_limit = 2
 
+    def get_iba_display(self, obj):
+        return obj.producto.get_iba_display()
 
-@admin.register(Registro_de_bajas)
+    get_iba_display.short_description = 'IBA'
+
+
+admin.site.register(Registro_de_altas, Registro_de_altasAdmin)
+
+
+
 class Registro_de_bajasAdmin(admin.ModelAdmin):
     list_display = ('producto', 'cantidad', 'fecha')
     list_display_links = ('cantidad', 'fecha')
+    list_filter = ('producto', 'fecha')
+    search_fields = ('producto__nombre',)
     list_display_links_limit = 2
+
+admin.site.register(Registro_de_bajas, Registro_de_bajasAdmin)
--- ERP_Project/models.py
+++ ERP_Project/models.py
@@ -1,4 +1,6 @@
 import random
+from datetime import date
+
 from django.db import models
 from django.db.models.signals import post_save
 from barcode import get_barcode_class
@@ -6,6 +8,7 @@ from barcode.writer import ImageWriter
 from django.core.files.base import ContentFile
 from io import BytesIO
 from django.dispatch import receiver
+
 
 
 class Categoria(models.Model):
@@ -17,12 +20,20 @@ class Categoria(models.Model):
 
 # This is model of Products
 class Producto(models.Model):
+    TIPO_CHOICES = (
+        (0, '0%'),
+        (4, '4%'),
+        (5, '5%'),
+        (10, '10%'),
+        (21, '21%')
+    )
     nombre = models.CharField(max_length=255)
     abreviaciones = models.CharField(max_length=10)
     categoria = models.ForeignKey(Categoria, null=True, on_delete=models.CASCADE)
     codigo = models.CharField(max_length=255, null=True, blank=True)
     codigo_barra = models.ImageField(upload_to='codigos_barra', null=True, blank=True)
     codigo_barra_number = models.CharField(max_length=255, null=True, blank=True)
+    iba = models.IntegerField(choices=TIPO_CHOICES)
 
     def __str__(self):
         return self.nombre
@@ -55,23 +66,39 @@ class Producto(models.Model):
 
 # This is model of inventary
 class Inventario(models.Model):
+    TIPO_UNIDADES = (
+        ('Gr', 'Gr'),
+        ('Kg', 'Kg'),
+        ('Ml', 'Ml'),
+        ('Lt', 'Lt'),
+        ('Cl', 'CL')
+    )
     producto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
+    cantidad = models.PositiveIntegerField(default=0)
+    unidad_medida = models.CharField(choices=TIPO_UNIDADES, max_length=10)
 
 
 # This model is for taking the products who are deregistered from the warehouse
 class Registro_de_bajas(models.Model):
     producto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
+    cantidad = models.PositiveIntegerField(default=0)
     fecha = models.DateField()
 
 
 class Registro_de_altas(models.Model):
     producto = models.ForeignKey(Producto, null=True, on_delete=models.CASCADE)
-    cantidad = models.IntegerField()
+    cantidad = models.PositiveIntegerField(default=0)
+    valor_unitario = models.PositiveIntegerField(default=0)
+    importe_total = models.PositiveIntegerField(default=0)
     fecha = models.DateField()
 
+    def save(self, *args, **kwargs):
+        self.importe_total = self.valor_unitario * self.cantidad + self.producto.iba
 
+        super().save(*args, **kwargs)
+
+
+# actualiza el inventario cuando se inserta un registro de altas
 @receiver(post_save, sender=Registro_de_altas)
 def actualizar_inventario_altas(sender, instance, created, **kwargs):
     if created:
@@ -79,11 +106,12 @@ def actualizar_inventario_altas(sender, instance, created, **kwargs):
         cantidad = instance.cantidad
 
         # Buscar o crear el registro de inventario
-        inventario, created = Inventario.objects.get_or_create(producto=producto)
+        inventario, created = Inventario.objects.get_or_create(producto=producto, defaults={'cantidad': 0})
         inventario.cantidad += cantidad
         inventario.save()
 
 
+# actualiza el inventario cuando se inserta un registro de bajas
 @receiver(post_save, sender=Registro_de_bajas)
 def actualizar_inventario_bajas(sender, instance, created, **kwargs):
     if created:
@@ -91,6 +119,8 @@ def actualizar_inventario_bajas(sender, instance, created, **kwargs):
         cantidad = instance.cantidad
 
         # Buscar o crear el registro de inventario
-        inventario, created = Inventario.objects.get_or_create(producto=producto)
+        inventario, created = Inventario.objects.get_or_create(producto=producto, defaults={'cantidad': 0})
         inventario.cantidad -= cantidad
         inventario.save()
+
+
